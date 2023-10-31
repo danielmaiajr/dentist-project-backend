8b7de5ad012d74239f61ee82ee639a15a406bc76
Daniel Maia <daniel.maia.jr12@gmail.com>
2023-10-31 16:10:20 -0300

    Simple routes for pacient added

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,40 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
 
 const app = fastify();
-app.get("/", () => {
-  return { user: "first" };
+
+const prisma = new PrismaClient();
+
+app.get("/pacients", async () => {
+  const pacients = await prisma.pacient.findMany();
+
+  return { pacients };
 });
 
-app.listen({ host: "0.0.0.0", port: 5000 }).then(() => {
-  console.log("Server running...");
+app.post("/pacients", async (request, reply) => {
+  const createUserSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+  });
+
+  const { name, email } = createUserSchema.parse(request.body);
+
+  await prisma.pacient.create({
+    data: {
+      name,
+      email,
+    },
+  });
+
+  return reply.status(201).send();
 });
+
+app
+  .listen({
+    host: "0.0.0.0",
+    port: process.env.PORT ? Number(process.env.PORT) : 5000,
+  })
+  .then(() => {
+    console.log("Server running...");
+  });
