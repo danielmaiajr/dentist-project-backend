f0143efaf2ecf993697d4075bbad8bb0c0fd0907
Daniel Maia <daniel.maia.jr12@gmail.com>
2023-11-06 10:47:16 -0300

    added authenticathe method to fastify server with the needed types

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,54 @@
 import fastify from "fastify";
-
+import fastifyJwt from "@fastify/jwt";
 import patientRoute from "./modules/patient/patient.route";
 import dentistRoute from "./modules/dentist/dentist.route";
 import userRoute from "./modules/user/user.route";
+import { FastifyRequest } from "fastify/types/request";
+import { FastifyReply } from "fastify/types/reply";
 
-const fastity = fastify();
+const fastifyApp = fastify();
 
-fastity.register(patientRoute, { prefix: "/api/patients" });
-fastity.register(dentistRoute, { prefix: "/api/dentists" });
-fastity.register(userRoute, { prefix: "/api/users" });
+declare module "@fastify/jwt" {
+  interface FastifyJWT {
+    payload: { id: number };
+    user: { id: number; name: string };
+  }
+}
+
+declare module "fastify" {
+  export interface FastifyInstance {
+    authenticate: any;
+  }
+}
+
+fastifyApp.register(fastifyJwt, {
+  secret: "supersecret",
+});
+
+fastifyApp.decorate(
+  "authenticate",
+  async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      await request.jwtVerify();
+    } catch (err) {
+      return reply.send(err);
+    }
+  }
+);
+
+fastifyApp.register(patientRoute, { prefix: "/api/patients" });
+fastifyApp.register(dentistRoute, { prefix: "/api/dentists" });
+fastifyApp.register(userRoute, { prefix: "/api/users" });
 
 const main = async () => {
   try {
-    fastity.listen({
+    fastifyApp.listen({
       host: "0.0.0.0",
       port: process.env.PORT ? Number(process.env.PORT) : 5000,
     });
     console.log("Server running...");
   } catch (err) {
-    fastity.log.error(err);
+    fastifyApp.log.error(err);
   }
 };
 
