98e258858cdc44004eca289fb20009b610dd992b
Daniel Maia <daniel.maia.jr12@gmail.com>
2023-11-06 13:04:51 -0300

    Remove prisma migrations from development stage, added prisma migration to .gitignore

--- prisma/migrations/20231031203137_create_patient_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Patient" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Patient_pkey" PRIMARY KEY ("id")
-);
