63c366bce25e85ac2845e961ec7a1ec039e65ad4
Daniel Maia <daniel.maia.jr12@gmail.com>
2023-11-06 14:49:07 -0300

    Updated dentist schema with zod library

--- src/modules/dentist/dentist.schema.ts
+++ src/modules/dentist/dentist.schema.ts
@@ -1,11 +1,41 @@
-export interface IParams {
-  dentistId: number;
-}
+import { z } from "zod";
 
-export interface ICreateDentist {
-  name: string;
-}
+// REQUEST Schema
+// POST /api/dentists
+export const CreateDentistRequestBodySchema = z.object({
+  name: z.string({
+    required_error: "Nome é Obrigatório",
+  }),
+});
 
-export interface IUpdateDentist {
-  name: string;
-}
+// REQUEST Schema
+// GET /api/dentists/:dentistId
+export const GetDentistByIdRequestParamsSchema = z.object({
+  dentistId: z
+    .string({
+      required_error: "dentistId é Obrigatório",
+    })
+    .transform((val) => Number(val)),
+});
+
+// REQUEST Schema
+// PUT /api/dentists/:dentistId
+export const PutDentistByIdRequestBodySchema = z.object(
+  {
+    name: z.string().optional(),
+  },
+  { required_error: "Precisa de pelo menos um campo" }
+);
+
+// TYPES
+export type CreateDentistRequestBodyType = z.infer<
+  typeof CreateDentistRequestBodySchema
+>;
+
+export type GetDentistByIdRequestParamsType = z.infer<
+  typeof GetDentistByIdRequestParamsSchema
+>;
+
+export type PutDentistByIdRequestBodyType = z.infer<
+  typeof PutDentistByIdRequestBodySchema
+>;
