82eec089998074262e7b0cce2f1a1633cbf6d6d1
Daniel Maia <daniel.maia.jr12@gmail.com>
2023-11-06 13:03:57 -0300

    Revert "Remove prisma migrations from development stage, added prisma migration to .gitignore"
    This reverts commit 2a612931ea42b9d0d8901747eaf1e7f3be3188e8.

---
+++ prisma/migrations/20231031203137_create_patient_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Patient" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Patient_pkey" PRIMARY KEY ("id")
+);
