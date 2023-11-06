692c128f3287be43950b7315d40ca7800aa925a3
Daniel Maia <daniel.maia.jr12@gmail.com>
2023-11-06 14:51:23 -0300

    Updated dentist controller to use the zod defined schemas

--- src/modules/dentist/dentist.controller.ts
+++ src/modules/dentist/dentist.controller.ts
@@ -1,59 +1,88 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import prisma from "../../utils/prisma";
-import { ICreateDentist, IParams, IUpdateDentist } from "./dentist.schema";
+import {
+  CreateDentistRequestBodySchema,
+  CreateDentistRequestBodyType,
+  GetDentistByIdRequestParamsSchema,
+  GetDentistByIdRequestParamsType,
+  PutDentistByIdRequestBodySchema,
+  PutDentistByIdRequestBodyType,
+} from "./dentist.schema";
 
 export async function createDentistHandler(
-  request: FastifyRequest<{ Querystring: ICreateDentist }>,
+  request: FastifyRequest<{ Body: CreateDentistRequestBodyType }>,
   reply: FastifyReply
 ) {
-  const { name } = request.query;
+  const parsedBody = CreateDentistRequestBodySchema.safeParse(request.body);
 
-  const dentist = await prisma.dentist.create({
-    data: {
-      name,
-    },
-  });
+  if (!parsedBody.success) return reply.send(parsedBody.error);
+  try {
+    const dentist = await prisma.dentist.create({
+      data: { ...parsedBody.data, userId: request.user.id },
+    });
 
-  reply.send(dentist);
+    reply.send(dentist);
+  } catch (err) {
+    reply.send(err);
+  }
+}
+
+export async function getDentistByIdHandler(
+  request: FastifyRequest<{ Params: GetDentistByIdRequestParamsType }>,
+  reply: FastifyReply
+) {
+  const parsedParams = GetDentistByIdRequestParamsSchema.safeParse(
+    request.params
+  );
+
+  if (!parsedParams.success) return reply.send(parsedParams.error);
+  try {
+    const dentist = await prisma.dentist.findUnique({
+      where: { id: parsedParams.data.dentistId },
+    });
+
+    reply.send(dentist);
+  } catch (err) {
+    reply.send(err);
+  }
 }
 
 export async function getAllDentistsHandler(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
-  const dentist = await prisma.dentist.findMany();
+  try {
+    const dentist = await prisma.dentist.findMany();
 
-  reply.send(dentist);
-}
-
-export async function getDentistByIdHandler(
-  request: FastifyRequest<{ Params: IParams }>,
-  reply: FastifyReply
-) {
-  console.log(request.params);
-  const { dentistId } = request.params;
-  const dentist = await prisma.dentist.findUnique({
-    where: { id: Number(dentistId) },
-  });
-
-  reply.send(dentist);
+    reply.send(dentist);
+  } catch (err) {
+    reply.send(err);
+  }
 }
 
 export async function updateDentistByIdHandler(
-  request: FastifyRequest<{ Params: IParams; Querystring: IUpdateDentist }>,
+  request: FastifyRequest<{
+    Params: GetDentistByIdRequestParamsType;
+    Body: PutDentistByIdRequestBodyType;
+  }>,
   reply: FastifyReply
 ) {
-  const { dentistId } = request.params;
-  const { name } = request.query;
+  const parsedParams = GetDentistByIdRequestParamsSchema.safeParse(
+    request.params
+  );
+  if (!parsedParams.success) return reply.send(parsedParams.error);
 
-  const data = {
-    ...(name && { name }),
-  };
+  const parsedBody = PutDentistByIdRequestBodySchema.safeParse(request.body);
+  if (!parsedBody.success) return reply.send(parsedBody.error);
 
-  const dentist = await prisma.dentist.update({
-    where: { id: Number(dentistId) },
-    data,
-  });
+  try {
+    const dentist = await prisma.dentist.update({
+      where: { id: parsedParams.data.dentistId },
+      data: parsedBody.data,
+    });
 
-  reply.send(dentist);
+    reply.send(dentist);
+  } catch (err) {
+    reply.send(err);
+  }
 }
