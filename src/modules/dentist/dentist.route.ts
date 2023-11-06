0651b70da358885e039d93558ea698349c0bf676
Daniel Maia <daniel.maia.jr12@gmail.com>
2023-11-06 14:23:13 -0300

    Added authetication to the needed dentists routes

--- src/modules/dentist/dentist.route.ts
+++ src/modules/dentist/dentist.route.ts
@@ -9,11 +9,24 @@ import {
 import { ICreateDentist, IParams, IUpdateDentist } from "./dentist.schema";
 
 async function routes(fastify: FastifyInstance, options: FastifyServerOptions) {
-  fastify.post<{ Querystring: ICreateDentist }>("/", createDentistHandler);
-  fastify.get("/", getAllDentistsHandler);
-  fastify.get<{ Params: IParams }>("/:dentistId", getDentistByIdHandler);
+  fastify.post<{ Querystring: ICreateDentist }>(
+    "/",
+    { onRequest: [fastify.authenticate] },
+    createDentistHandler
+  );
+  fastify.get(
+    "/",
+    { onRequest: [fastify.authenticate] },
+    getAllDentistsHandler
+  );
+  fastify.get<{ Params: IParams }>(
+    "/:dentistId",
+    { onRequest: [fastify.authenticate] },
+    getDentistByIdHandler
+  );
   fastify.put<{ Params: IParams; Querystring: IUpdateDentist }>(
     "/:dentistId",
+    { onRequest: [fastify.authenticate] },
     updateDentistByIdHandler
   );
 }
