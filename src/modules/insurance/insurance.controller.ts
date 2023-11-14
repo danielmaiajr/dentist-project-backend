1af9720034503e09b1e364aacf9e046e2c252333
Daniel Maia <daniel.maia.jr12@gmail.com>
2023-11-14 13:03:48 -0300

    Added createInsuranceHandler with respective body schema

--- src/modules/insurance/insurance.controller.ts
+++ src/modules/insurance/insurance.controller.ts
@@ -1,11 +1,27 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { CreateInsuranceRequestBodySchema } from "./insurance.schema";
+import prisma from "../../utils/prisma";
 
 // POST /api/insurances
 export async function createInsuranceHandler(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
-  return reply.send("createInsuranceHandler");
+  const parsedBody = CreateInsuranceRequestBodySchema.safeParse(request.body);
+  if (!parsedBody.success) return reply.code(500).send(parsedBody.error);
+
+  try {
+    const insurance = await prisma.insurance.create({
+      data: {
+        ...parsedBody.data,
+        clinicId: request.user.clinicId,
+      },
+    });
+
+    return reply.send(insurance);
+  } catch (err) {
+    return reply.code(500).send({ errorMessage: "database Error", err });
+  }
 }
 
 // GET /api/insurances
--- src/modules/insurance/insurance.schema.ts
+++ src/modules/insurance/insurance.schema.ts
@@ -2,10 +2,28 @@ import { z } from "zod";
 
 // ----- REQUEST SCHEMA -----
 // POST /api/insuranses
+export const CreateInsuranceRequestBodySchema = z.object({
+  name: z.string({
+    required_error: "Nome é Obrigatório",
+  }),
+});
+
 // GET /api/insuranses
 // PUT /api/insuranses
+export const PutInsuranceRequestBodySchema = z.object({
+  name: z.string().optional(),
+});
 
 // ----- REPLY SCHEMA -----
 // POST /api/insuranses
 // GET /api/insuranses
 // PUT /api/insuranses
+
+// TYPES
+export type CreateInsuranceRequestBodyType = z.infer<
+  typeof CreateInsuranceRequestBodySchema
+>;
+
+export type PutInsuranceRequestBodyType = z.infer<
+  typeof PutInsuranceRequestBodySchema
+>;
